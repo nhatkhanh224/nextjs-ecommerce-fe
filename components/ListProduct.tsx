2f91d7aa1cb2dc3d9132b09842a675a2be56b2d7
nhatkhanh224 <55695197+nhatkhanhl@users.github.com>
2023-04-08 20:48:59 +0700

    First commit

---
+++ components/ListProduct.tsx
@@ -0,0 +1,57 @@
+export default function ListProduct() {
+  return (
+    <>
+    <div className="product-wrapper mt-5">
+        <div class="container mx-auto px-4">
+          <div class="grid grid-cols-4 gap-4">
+            <div className="product-item">
+              <img src="http://smakerclothing.com/upload/sanpham/dsc05779-8029.jpg"></img>
+              <p className="text-center font-bold">TEE</p>
+              <p className="text-center">100.000 VND</p>
+            </div>
+            <div className="product-item">
+              <img src="http://smakerclothing.com/upload/sanpham/dsc05779-8029.jpg"></img>
+              <p className="text-center font-bold">TEE</p>
+              <p className="text-center">100.000 VND</p>
+            </div>
+            <div className="product-item">
+              <img src="http://smakerclothing.com/upload/sanpham/dsc05779-8029.jpg"></img>
+              <p className="text-center font-bold">TEE</p>
+              <p className="text-center">100.000 VND</p>
+            </div>
+            <div className="product-item">
+              <img src="http://smakerclothing.com/upload/sanpham/dsc05779-8029.jpg"></img>
+              <p className="text-center font-bold">TEE</p>
+              <p className="text-center">100.000 VND</p>
+            </div>
+            <div className="product-item">
+              <img src="http://smakerclothing.com/upload/sanpham/dsc05779-8029.jpg"></img>
+              <p className="text-center font-bold">TEE</p>
+              <p className="text-center">100.000 VND</p>
+            </div>
+            <div className="product-item">
+              <img src="http://smakerclothing.com/upload/sanpham/dsc05779-8029.jpg"></img>
+              <p className="text-center font-bold">TEE</p>
+              <p className="text-center">100.000 VND</p>
+            </div>
+            <div className="product-item">
+              <img src="http://smakerclothing.com/upload/sanpham/dsc05779-8029.jpg"></img>
+              <p className="text-center font-bold">TEE</p>
+              <p className="text-center">100.000 VND</p>
+            </div>
+            <div className="product-item">
+              <img src="http://smakerclothing.com/upload/sanpham/dsc05779-8029.jpg"></img>
+              <p className="text-center font-bold">TEE</p>
+              <p className="text-center">100.000 VND</p>
+            </div>
+          </div>
+          <div className="flex justify-center mt-4 mb-4">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+              SEE MORE
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
